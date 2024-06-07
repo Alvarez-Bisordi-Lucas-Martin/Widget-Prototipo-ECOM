5e1aa5ed7e77db323bba4ae678d454ae01f65495
Lucas Martin Alvarez Bisordi <129774135+Alvarez-Bisordi-Lucas-Martin@users.noreply.github.com>
2024-06-07 01:43:58 -0300

    Add files via upload

---
+++ WidgetPrototipo/Aplicaciones.js
@@ -0,0 +1,10 @@
+var app = {
+    id: '',
+    nombre: ''
+};
+
+var apps = [
+    { id: '1', nombre: 'EcomWeb' },
+    { id: '2', nombre: 'LoginPageTest' },
+    { id: '3', nombre: 'TicketApp' }
+];
