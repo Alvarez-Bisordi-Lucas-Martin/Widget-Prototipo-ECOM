123fb95368fecf043690aec520006c2ee16dd101
Lucas Martin Alvarez Bisordi <129774135+Alvarez-Bisordi-Lucas-Martin@users.noreply.github.com>
2024-09-13 10:21:39 -0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,13 @@
+var config = {
+    client_id: 'tixGF8JbWPuigMMDkYTFfL2tFSrIyqre2mP332j0',
+    client_secret: 'pbkdf2_sha256$720000$6q1Ug2Y7C8S7PsAYD1MRPY$xadVS+y0mBErDvlY539JFMRJoeUZ75Og4vPmvzDG1Kg=',
+    app_name: 'Turismo (Aplicación)',
+    flujo_id: '28a28f32-3cf2-4a6a-807e-074cb445d170',
+    metadata: {
+        username: 'LucasBisordi',
+        first_name: 'Lucas',
+        last_name: 'Bisordi',
+        fecha_hora: new Date()
+    },
+    entorno_actual: 'local'
+};
